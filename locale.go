6fe647e4b6d252f356fea8ec80168a41a5e7a9aa
rminnich@gmail.com <rminnich@gmail.com>
2010-12-15 11:49:52 -0800

    initial strongbox support. This compiles. Testing later.

--- locale.go
+++ locale.go
@@ -9,7 +9,21 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"net"
+	"os"
 )
+
+func getOurIPs() ([]string) {
+	hostName, err := os.Hostname()
+	if err != nil {
+		log.Exit(err)
+	}
+	_, addrs, err := net.LookupHost(hostName)
+	if err != nil {
+		log.Exit(err)
+	}
+	return addrs
+}
 func localeInit() {
 	switch {
 	case *locale == "local":
@@ -21,6 +35,30 @@ func localeInit() {
 				log.Exit(err)
 			}
 			cmdSocket = "127.0.0.1:" + string(cmd)
+		case role == "client":
+		case role == "run":
+		}
+	case *locale == "strongbox":
+		addrs := getOurIPs()
+		switch {
+		case role == "master":
+			cmdPort = "6666"
+		case role == "slave":
+			cmdPort = "6666"
+			/* on strongbox there's only ever one.
+			 * pick out the lowest-level octet.
+			 */
+			b := net.ParseIP(addrs[0]).To4()
+			which := b[3]
+			switch {
+			case which % 7 == 0:
+				cmdSocket = "10.0.0.254:6666"
+			default: 
+				boardMaster := ((which + 6) / 7) * 7
+				cmdSocket = "10.0.0." + string(boardMaster) + ":6666"
+			}
+			
+			cmdSocket = addrs[0] + ":" + "6666"
 		case role == "client":
 		case role == "run":
 		}
