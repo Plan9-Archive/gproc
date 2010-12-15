c6c58eedb219e8ad4ce48cee476011e8bee3ebc9
rminnich@gmail.com <rminnich@gmail.com>
2010-12-14 17:26:39 -0800

    locale support

---
+++ locale.go
@@ -0,0 +1,15 @@
+/* these are variables which it makes no sense to have as options. 
+ * at the same time, a json-style file makes no sense either; we have to carry it along and it 
+ * does not express computation well. They are determined from your 
+ * location and in many cases they will end up being computed. 
+ */
+
+package main
+
+func localeInit() {
+
+	switch {
+//		case locale == "strongbox"
+		case *locale == "":	
+	}
+}
