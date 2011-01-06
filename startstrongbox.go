87ee6d676eee1d09570db7f5eacda63ee252ca84
rminnich@gmail.com <rminnich@gmail.com>
2011-01-06 15:09:35 -0800

    Start things up. We should not be using shell scripts :-)
    using on strongbox:
    ./startstrongbox -l 1 -h 196 1
    
    This gets the first level, i.e. nodes that are multiple of 7
    
    ./startstrongbox -l 1 -h 196 2
    
    This gets second level, i.e. nodes that are all the others.

--- startstrongbox.go
+++ startstrongbox.go
@@ -23,33 +23,45 @@ func usage() {
 var (
 	lowNode	=	flag.Int("l", 1, "Lowest node number")
 	highNode=flag.Int("h", 196, "Highest node number")
+	debugLevel = flag.Int("d", 0, "Debug level")
 )
 
 func runlevel(lowNode, highNode int, mod7 bool) {
+	reap := make(chan *os.Waitmsg, 0)
+	numspawn := 0
 	for i := lowNode; i <= highNode; i++ {
 		fmt.Printf("Check %d; mode %v; mod7 %v\n", i, (i %7 == 0), mod7)
 		if (i % 7 == 0) != mod7  {
 			continue
 		}
+		numspawn++
 		go func(anode int) {
 			node := fmt.Sprintf("root@cn%d", anode)
 			
-			Args := []string{"ssh", "-o", "StrictHostKeyCHecking=no", node, "./gproc_linux_arm", "-locale=strongbox", "s"}
+			Args := []string{"ssh", "-o", "StrictHostKeyCHecking=no", node, "./gproc_linux_arm", "-locale=strongbox", fmt.Sprintf("-debug=%d", *debugLevel), "s"}
 			f := []*os.File {nil, os.Stdout, os.Stderr}
 			fmt.Printf("Spawn to %v\n", node)
-			_, err := os.ForkExec("/usr/bin/ssh", Args, os.Environ(), "", f)
+			pid, err := os.ForkExec("/usr/bin/ssh", Args, os.Environ(), "", f)
 			if err != nil {
 				fmt.Print("Forkexec fails: ", err)
 			}
+			
+			msg, err := os.Wait(pid, 0)
+			reap <- msg
 		}(i)
+	}
+	for numspawn > 0 {
+		msg := <- reap
+		fmt.Printf("Reaped %v\n", msg)
+		numspawn--
 	}
 }
 func main() {
 	flag.Usage = usage
 	flag.Parse()
+	/* use 1 for the top level. Use anything else for the next level down */
+	level1 := flag.Args()[0] == "1"
 	fmt.Printf("Start nodes %d to %d\n", *lowNode, *highNode)
-	runlevel(*lowNode, *highNode, true)
-	fmt.Printf("Start nodes %d to %d\n", *lowNode, *highNode)
-	runlevel(*lowNode, *highNode, false)
+	runlevel(*lowNode, *highNode, level1)
 }
 
