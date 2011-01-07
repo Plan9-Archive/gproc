b5cc7706ab8b2b938fbb30d0a6352d3b56c80a18
rminnich@gmail.com <rminnich@gmail.com>
2011-01-07 08:43:47 -0800

    simplify strongboxcfg.go

--- strongboxcfg.go
+++ strongboxcfg.go
@@ -11,8 +11,6 @@ package main
 
 import (
 	"os"
-	"net"
-	"log"
 	"strconv"
 	"strings"
 )
@@ -22,44 +20,14 @@ type strongbox struct {
 	parentAddr string
 	ip string
 	addr string // consider a better name
-	hostMap map[string][]string
-	idMap map[string] string
 }
 
 func init() {
 	addLocale("strongbox", new(strongbox))
 }
 
-func (s *strongbox) getIPs() []string {
-	hostName, err := os.Hostname()
-	if err != nil {
-		log.Exit(err)
-	}
-	if addrs, ok := s.hostMap[hostName]; ok {
-		return addrs
-	}
-	_, addrs, err := net.LookupHost(hostName)
-	if err != nil {
-		log.Exit(err)
-	}
-	return addrs
-}
-
-func (s *strongbox) initHostTable() {
-	s.hostMap = make(map[string][]string)
-	s.idMap = make(map[string]string)
-	for i := 0; i < 197; i++ {
-		n := strconv.Itoa(i)
-		host := "cn" + n
-		ip := "10.0.0." + n
-		s.hostMap[host] = []string{ip}
-		s.idMap[ip] = strconv.Itoa(i)
-	}
-}
-
+/* convention: strongbox nodes are named "cn" */
 func (s *strongbox) Init(role string) {
-		s.initHostTable()
-		addrs := s.getIPs()
 		switch role {
 		case "master":
 			cmdPort = "6666"
@@ -74,8 +42,8 @@ func (s *strongbox) Init(role string) {
 			/* on strongbox there's only ever one.
 			 * pick out the lowest-level octet.
 			 */
-			b := net.ParseIP(addrs[0]).To4()
-			which := b[3]
+			hostname,_ := os.Hostname()
+			which,_ := strconv.Atoi(hostname[2:])
 			switch {
 			case which%7 == 0:
 				s.parentAddr = *parent + ":6666"
@@ -83,7 +51,7 @@ func (s *strongbox) Init(role string) {
 				boardMaster := ((which + 6) / 7) * 7
 				s.parentAddr = "10.0.0." + strconv.Itoa(int(boardMaster)) + ":6666"
 			}
-			s.ip = b.String()
+			s.ip = "10.0.0." + strconv.Itoa(which)
 			s.addr = s.ip + ":" + cmdPort
 		case "client":
 		}
@@ -104,7 +72,14 @@ func (s *strongbox) Ip() string {
 func (s *strongbox) SlaveIdFromVitalData(vd *vitalData) (id string) {
 	/* grab the server address from vital data and index into our map */
 	addrs := strings.Split(vd.ServerAddr, ":", 2)
-	id = s.idMap[addrs[0]]
+	octets := strings.Split(addrs[0], ".", 4)
+	which,_ := strconv.Atoi(octets[3])
+	/* get the lowest octet, take it mod 7 */
+	if which % 7 == 0 {
+		id = strconv.Itoa(which / 7)
+	} else {
+		id = strconv.Itoa(which % 7)
+	}
 	return
 }
 
