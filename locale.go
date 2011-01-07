8eef83daf3ffff592740475e55add608a0661db7
rminnich@gmail.com <rminnich@gmail.com>
2011-01-07 14:58:25 -0800

    Turn off prints

--- locale.go
+++ locale.go
@@ -72,10 +72,10 @@ var (
 )
 
 func newLocale(name string) (loc Locale, err os.Error) {
-	log.Print(locales)
+	Dprint(8, locales)
 	var inLocales bool
 	if loc, inLocales = locales[name]; inLocales {
-		log.Print("found ", name)
+		Dprint(8, "found ", name)
 		return
 	}
 	if _, err = os.Lstat(name); err != nil {
