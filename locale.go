e599b20b60a6269f2f5ac6b7e23cf231635b3811
John Floren <john@jfloren.net>
2011-03-09 14:08:56 -0800

    Updated license

--- locale.go
+++ locale.go
@@ -1,7 +1,7 @@
 /*
  * gproc, a Go reimplementation of the LANL version of bproc and the LANL XCPU software. 
  * 
- * This software is released under the Lesser Gnu Programming License, incorporated herein by reference. 
+ * This software is released under the GNU Lesser General Public License, version 2, incorporated herein by reference. 
  *
  * Copyright (2010) Sandia Corporation. Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation, 
  * the U.S. Government retains certain rights in this software.
